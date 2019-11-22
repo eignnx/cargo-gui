cd92ce0c0a520dc297fe691afa21e06b27d9a90c
eignnx <eignnx@gmail.com>
2019-11-21 21:09:09 -0500

    add margin bottom to title block

--- public/js/project-title.js
+++ public/js/project-title.js
@@ -1,7 +1,7 @@
 const projectTitle = Vue.component("project-title", {
     props: ["title", "path"],
     template: `
-    <header>
+    <header class="mb-5">
         <h1 class="display-1" id="project-title">{{ title }}</h1>
         <h3 class="text-muted">{{ path }}</h3>
     </header>
