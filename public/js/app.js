80f18970eb4194b1e1f71f56254246f193f64a60
eignnx <eignnx@gmail.com>
2019-12-09 20:56:08 -0500

    remove unneeded setTimeout

--- public/js/app.js
+++ public/js/app.js
@@ -105,7 +105,7 @@ const app = new Vue({
         .then(json => {
           if (json.hasOwnProperty("line")) {
             this[`${stream_name}Lines`].push(json["line"]);
-            setTimeout(() => this.readNextLine(stream_name), 0);
+            this.readNextLine(stream_name);
           } else if (json === "end") {
             this[`${stream_name}LinesDone`] = true;
             if (!this.cmdRunning) {
