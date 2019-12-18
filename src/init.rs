ad84b2d8ec71eec3978bfdd5cb20f043e4d78856
eignnx <eignnx@gmail.com>
2019-12-17 21:52:45 -0500

    allow windows users to enter npm.cmd path if it can't be found

--- src/init.rs
+++ src/init.rs
@@ -15,13 +15,24 @@ pub fn init_js_app(home_dir: impl AsRef<Path>) {
             panic!("`npm install` failed to run!");
         }
     } else if cfg!(target_os = "windows") {
-        let output = Command::new(r"C:\Windows\System32\where.exe")
-            .arg("npm")
-            .output()
-            .expect("`where npm` ran successfully");
-
-        let output = std::str::from_utf8(&output.stdout).unwrap();
-        let path_to_npm = output.lines().nth(1).unwrap();
+        // Attempt to use `where` to locate `npm.cmd`.
+        let where_path = r"C:\Windows\System32\where.exe";
+        let path_to_npm = match Command::new(where_path).arg("npm").output() {
+            Ok(ref output) => {
+                let bytes = &output.stdout;
+                let output = std::str::from_utf8(bytes).unwrap();
+                output.lines().nth(1).unwrap().to_owned()
+            }
+            Err(_) => {
+                // Allow the user to enter the path to their npm.cmd file if it cannot be located automatically.
+                eprint!("Could not locate npm! Please enter the path to the `npm.cmd` file installed on your system: ");
+                let mut buf = String::new();
+                std::io::stdin()
+                    .read_line(&mut buf)
+                    .expect("stdin could not be read from");
+                buf.trim().to_owned()
+            }
+        };
 
         let status = Command::new(path_to_npm)
             .arg("install")
