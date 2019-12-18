6551e3578326ded20211f3f86522a5685cbeac1a
eignnx <eignnx@gmail.com>
2019-12-17 20:44:40 -0500

    allow npm to run on windows

--- src/main.rs
+++ src/main.rs
@@ -157,14 +157,35 @@ async fn start_running_cargo_cmd(mut req: Req) -> tide::Response {
 }
 
 fn init_js_app(home_dir: impl AsRef<Path>) {
-    let status = Command::new("npm")
-        .arg("install")
-        .current_dir(home_dir.as_ref().join("public"))
-        .status()
-        .expect("`npm install` will run successfully");
+    let wd = std::fs::canonicalize(home_dir.as_ref().join("public")).unwrap();
+    if cfg!(not(target_os = "windows")) {
+        let status = Command::new("npm")
+            .arg("install")
+            .current_dir(wd)
+            .status()
+            .expect("`npm install` will run successfully");
 
-    if !status.success() {
-        panic!("`npm install` failed to run!");
+        if !status.success() {
+            panic!("`npm install` failed to run!");
+        }
+    } else if cfg!(target_os = "windows") {
+        let output = Command::new(r"C:\Windows\System32\where.exe")
+            .arg("npm")
+            .output()
+            .expect("`where npm` ran successfully");
+
+        let output = std::str::from_utf8(&output.stdout).unwrap();
+        let path_to_npm = output.lines().nth(1).unwrap();
+
+        let status = Command::new(path_to_npm)
+            .arg("install")
+            .current_dir(wd)
+            .status()
+            .expect("`npm install` will run successfully");
+
+        if !status.success() {
+            panic!("`npm install` failed to run!");
+        }
     }
 }
 
