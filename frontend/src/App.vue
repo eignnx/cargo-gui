61cd6f3d7462443926ba1fd61b56491b845ac679
eignnx <eignnx@gmail.com>
2019-12-19 10:32:48 -0500

    include bootstrap

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -179,12 +179,4 @@ function defer() {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
